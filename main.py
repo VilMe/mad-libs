b012933d71107eefbd213db3f3d9bd18896bb060
VilMe <vilma.medrano00@gmail.com>
2025-09-01 21:32:20 -0500

    define variables for word types that i want to get from user

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ def get_input(word_type: str):
     user_input: str = input(f"Enter a {word_type}:")
     return user_input
 
+
+noun1 = get_input("noun")
+verb1 = get_input("verb")
+noun2 = get_input("noun")
+verb2 = get_input("verb")
+
+
