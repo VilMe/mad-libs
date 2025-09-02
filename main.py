0e77b05547d588069df3737a18431935e31be862
VilMe <vilma.medrano00@gmail.com>
2025-09-01 21:41:21 -0500

    add story for the madlibs :)

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ def get_input(word_type: str):
     return user_input
 
 
-noun1 = get_input("noun")
+adjective1 = get_input("adjective")
+noun1 = get_input("proper noun")
 verb1 = get_input("verb")
-noun2 = get_input("noun")
+noun2 = get_input("proper noun")
 verb2 = get_input("verb")
 
 
+story = f"""
+
+Once upon a time, there was a {adjective1} {noun1} who love to {verb1} all day.
+
+One day, {noun2} walked into the roome and caught {noun1} in the act.
+
+{noun2}: "What are you doing?"
+{noun1}: "I'm just {verb1}ing, what's the big deal?"
+{noun2}: "Well, it's not every day tha you see a {noun1} {verb1}ing in the middle of the day"
+{noun1}: "I guess you're right. Maybe I should take a break"
+{noun2}: "That's probably a good idea. Why don'e we go {verb2} instead?"
+{noun1}: "Sure, that sounds like fun!"
+
+And so, {noun2} and the {noun1} went off to {verb2} and had a great time.
+The end. 
+"""
